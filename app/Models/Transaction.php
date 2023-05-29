d3b0fdd3047cd2ddc90a4b65ae416f395630f743
mahdyyy93 <salahthabetmahdy@gmail.com>
2023-05-29 14:15:47 +0300

    add scopeFilterByUser to Transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Transaction extends Model implements Auditable
 {
@@ -23,6 +24,15 @@ class Transaction extends Model implements Auditable
         return $this->hasOne(Status::class);
     }
 
+    public function scopeFilterByUser($query)
+    {
+        if (Auth::user() && !Auth::user()->is_admin) {
+            return $query->where('user_id', Auth::user()->id);
+        }
+
+        return $query;
+    }
+    
     public function getCodeAttribute()
     {
         return strtoupper($this->attributes['code']);

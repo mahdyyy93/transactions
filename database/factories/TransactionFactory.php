b18c177813aac09b9b44e02ac7645c89a0ea507d
mahdyyy93 <salahthabetmahdy@gmail.com>
2023-05-30 19:39:01 +0300

    getting transactions

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'code' => Str::random(6),
+            'amount' => rand(10,100000),
+            'status_id' => rand(1,3),
+        ];
+    }
+}
---
+++ tests/Feature/UserListTransactionsTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Transaction;
+use Database\Seeders\StatusSeeder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserListTransactionsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public Collection $users;
+    public User $admin;
+
+    public function setUp(): void 
+    {
+        parent::setUp();
+
+        $this->seed(StatusSeeder::class);
+        $this->users = User::factory()->count(2)->create();
+        $this->admin = User::factory()->create(['is_admin' => true]);
+        Transaction::factory()->count(2)->create(['user_id'=>$this->users[0]->id]);
+        Transaction::factory()->count(3)->create(['user_id'=>$this->users[1]->id]);
+    }
+
+    public function test_first_user_gets_his_transaction(): void
+    {
+        $response = $this->actingAs($this->users[0])->get('/api/transactions');
+
+        $response->assertJsonCount(2, 'data');
+        $response->assertStatus(200);
+    }
+
+    public function test_admin_gets_transaction_of_all_users(): void
+    {
+        $response = $this->actingAs($this->admin)->get('/api/transactions');
+
+        $response->assertJsonCount(5, 'data');
+        $response->assertStatus(200);
+    }
+}

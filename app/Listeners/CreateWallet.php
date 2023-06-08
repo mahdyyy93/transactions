11304a99f6b24fbd7b02f5605af1da3ece5bf568
mahdyyy93 <salahthabetmahdy@gmail.com>
2023-06-08 20:21:19 +0300

    CreateWallet listener to user registeration

--- app/Listeners/CreateWallet.php
+++ app/Listeners/CreateWallet.php
@@ -7,15 +7,15 @@ use Illuminate\Auth\Events\Registered;
 
 class CreateWallet
 {
-    public WalletService $wallet_service;
+    public function __construct(
+        public WalletService $wallet_service
+    ) {}
 
     /**
      * Handle the event.
      */
     public function handle(Registered $event): void
     {
-
-        $this->wallet_service = new WalletService($event->user);
-        $this->wallet_service->create();
+        $this->wallet_service->create($event->user->id);
     }
 }

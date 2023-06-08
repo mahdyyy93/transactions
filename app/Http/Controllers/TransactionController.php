5a63334f0f17612686ac99f94aef6b6eac9ac30b
mahdyyy93 <salahthabetmahdy@gmail.com>
2023-06-08 20:20:31 +0300

    clean TransactionController

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Transaction;
-use Illuminate\Http\Request;
 use App\DTOs\TransactionInitiate;
-use App\Services\Transactions\Get;
 use App\Services\Transactions\Initiate;
+use App\Services\Wallets\WalletService;
 use App\Http\Resources\TransactionResource;
 use App\Http\Requests\TransactionIndexRequest;
 use App\Http\Requests\TransactionCreateRequest;
@@ -14,7 +13,8 @@ use App\Http\Requests\TransactionCreateRequest;
 class TransactionController extends Controller
 {
     public function __construct(
-        public Initiate $transactionInitiate
+        public Initiate $transactionInitiate,
+        public WalletService $wallet_service
     ) {}
 
     /**
@@ -36,52 +36,12 @@ class TransactionController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create(TransactionCreateRequest $request)
+    public function store(TransactionCreateRequest $request)
     {
         return TransactionResource::make(
             $this->transactionInitiate->create(
                 TransactionInitiate::fromRequest($request)
             )
         );
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Transaction $transaction)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Transaction $transaction)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Transaction $transaction)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Transaction $transaction)
-    {
-        //
     }
 }
